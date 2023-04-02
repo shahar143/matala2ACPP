df1dc8a9571ae387c55ad547177989c294f3fa95
super <shaharuli52@gmail.com>
2023-04-02 14:38:30 +0300

    part b

--- sources/card.hpp
+++ sources/card.hpp
@@ -6,7 +6,32 @@
 #define CARDWAR_A_CARD_HPP
 
 namespace ariel{
-    class Card {
+    class Card{
+        int card_type;
+
+        Card(const int& card){
+            if(card < 1 || card > 13){
+                std::cout << "not a valid card number" << std::endl;
+                throw std::exception();
+            }
+            this->card_type = card;
+        }
+        int compareTo(const Card& other){
+            if(this->card_type == 1){
+                if(other.card_type != 2 && other.card_type != 1) return 1;
+                else if(other.card_type == 2) return -1;
+                else if(other.card_type == 1) return 0;
+            }
+            else if(other.card_type == 1){
+                if(this->card_type != 2) return -1;
+                else return 1;
+            }
+            else{
+                if(this->card_type > other.card_type) return 1;
+                else if(this->card_type < other.card_type) return -1;
+                else return 0;
+            }
+        }
 
     };
 }
